35eb1ff2d1078107d025394b8b74620589a33788
Felix Cervin <fexellofficial@gmail.com>
2025-05-03 16:14:41 +0200

    Removed app.vue code

--- app.vue
+++ app.vue
@@ -1,21 +1,11 @@
 <script setup>
-  let store
 
-  if(import.meta.client) {
-    store                                   = reactive({ isDesktop: window.matchMedia('(min-width: 767px)').matches })
-
-    window.addEventListener('resize', () => store.isDesktop = window.matchMedia('(min-width: 767px)').matches)
-  }
 </script>
 
 <template>
   <main
     id='App'
-    class='app'
-    :class='{
-      "is-desktop": store.isDesktop,
-      "is-mobile": !store.isDesktop
-    }'>
+    class='app'>
     <div
       class='app-container'>
       <NuxtLayout />
